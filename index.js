b3811f13175d0495f604d245f620c72fc7476042
Lenna <lenna@lennamau.com>
2019-02-11 17:07:39 -0700

    get request

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 // import your node modules
-const express = require('express');
-const db = require('./data/db.js');
+const express = require("express");
+const db = require("./data/db.js");
 
 const server = express();
 
 server.use(express.json());
 
-
 // add your server code starting here
 
-server.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+server.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-  server.get('/api/posts', (req, res) => {
-      db.find()
-      .then(posts => {
-          res.status(200).json( { success: true, posts})
-      })
-      .catch(err => {
-          res.status(err.code).json({ success: false, message: err.message })
-      })
-  });
-       
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json({ posts });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+server.get("/api/posts/:user", (req, res) => {
+  const id = req.params.user;
+
+  db.findById(id)
+    .then(posts => {
+      if (posts.length) {
+        res.status(200).json({ posts });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
 
 
 server.listen(4000, () => {
-    console.log('\n*** Running on port 4000 ***\n');
-  });
+  console.log("\n*** Running on port 4000 ***\n");
+});
